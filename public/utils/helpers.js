e9ef8e49b4856abab37d3ae7bce8080e9cacd9b5
Dracaukwak <43927819+Dracaukwak@users.noreply.github.com>
2025-04-22 15:56:59 +0200

    Revert "Restructure project using MVC pattern and modular CSS"
    This reverts commit 9e945e6362291d6cb09e44cb1134bacd9f81be4c.

--- public/utils/helpers.js
+++
@@ -1,6 +0,0 @@
- 
-export function escapeHtml(unsafe) {
-    return String(unsafe || '').replace(/[&<>"']/g, m => ({
-        '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#039;'
-    })[m]);
-}
