6e782485370b23b10306bbbf398c12f58899276c
Dracaukwak <43927819+Dracaukwak@users.noreply.github.com>
2025-04-23 10:53:29 +0200

    Add comment to escapeHtml utility function for clarity

--- public/utils/helpers.js
+++ public/utils/helpers.js
@@ -1,6 +1,14 @@
- 
+/**
+ * Escapes special HTML characters in a string to prevent XSS attacks
+ * @param {string} unsafe - The raw string that may contain HTML
+ * @returns {string} - The sanitized string with HTML entities
+ */
 export function escapeHtml(unsafe) {
     return String(unsafe || '').replace(/[&<>"']/g, m => ({
-        '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#039;'
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
     })[m]);
 }
